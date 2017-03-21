0dde6807862ef25c73113ef83836f076dc4d869b
Sultan Alsawaf <sultan@kerneltoast.com>
2017-03-21 10:42:56 -0700

    msm: camera: isp: Fix memory leaks in vfe probe
    Memory is needlessly allocated to vfe_parent_dev and vfe_parent_dev->common_sd
    and never freed.
    
    Since the allocated memory is not used anywhere outside of vfe_probe(),
    and since it's unnecessary, just remove the allocations entirely and use a
    stack variable instead.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/media/platform/msm/camera_v2/isp/msm_isp.c
+++ drivers/media/platform/msm/camera_v2/isp/msm_isp.c
@@ -1,4 +1,4 @@
-/* Copyright (c) 2013-2018, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2013-2017, The Linux Foundation. All rights reserved.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 and
@@ -553,21 +553,20 @@ static int vfe_probe(struct platform_device *pdev)
 		if (!node) {
 			pr_err("%s: Error! Cannot find node in dtsi %s\n",
 				__func__, name);
-			goto end;
+			break;
 		}
 		new_dev = of_find_device_by_node(node);
 		if (!new_dev) {
 			pr_err("%s: Failed to find device on bus %s\n",
 				__func__, node->name);
-			goto end;
+			break;
 		}
 		new_dev->dev.platform_data = &vfe_common_data;
 		rc = vfe_set_common_data(new_dev);
 		if (rc < 0)
-			goto end;
+			break;
 	}
 
-end:
 	return rc;
 }
 
@@ -652,8 +651,6 @@ int vfe_hw_probe(struct platform_device *pdev)
 	spin_lock_init(&vfe_dev->shared_data_lock);
 	spin_lock_init(&vfe_dev->reg_update_lock);
 	spin_lock_init(&req_history_lock);
-	spin_lock_init(&vfe_dev->reset_completion_lock);
-	spin_lock_init(&vfe_dev->halt_completion_lock);
 	media_entity_init(&vfe_dev->subdev.sd.entity, 0, NULL, 0);
 	vfe_dev->subdev.sd.entity.type = MEDIA_ENT_T_V4L2_SUBDEV;
 	vfe_dev->subdev.sd.entity.group_id = MSM_CAMERA_SUBDEV_VFE;
