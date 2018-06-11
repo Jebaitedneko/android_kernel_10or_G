4dccb78deb4d7e55a7561fc7b9cb1b10c52ef0c6
RAKESH ROSHAN <rakeshraimca@gmail.com>
2018-06-11 14:21:29 +0530

    added underclock cpu frequency upto 420 mhz

--- drivers/clk/msm/clock-cpu-8953.c
+++ drivers/clk/msm/clock-cpu-8953.c
@@ -132,7 +132,7 @@ static struct pll_clk apcs_hf_pll = {
 	},
 	.base = &virt_bases[APCS_C0_PLL_BASE],
 	.max_rate = 2400000000UL,
-	.min_rate = 652800000UL,
+	.min_rate = 422400000UL,
 	.src_rate =  19200000UL,
 	.c = {
 		.parent = &xo_a_clk.c,
