88d981ab166253b6980442977e0b559d0db41ea8
RAKESH ROSHAN <rakeshraimca@gmail.com>
2018-07-14 11:04:15 +0530

    revert power suspend mode

--- include/linux/powersuspend.h
+++ include/linux/powersuspend.h
@@ -37,7 +37,14 @@
 #define POWER_SUSPEND_USERSPACE	1	// Use fauxclock as trigger
 #define POWER_SUSPEND_PANEL	2	// Use display panel state as hook
 #define POWER_SUSPEND_HYBRID	3	// Use display panel state and autosleep as hook
+enum {
 
+	POWER_SUSPEND_LEVEL_BLANK_SCREEN = 50,
+
+	POWER_SUSPEND_LEVEL_STOP_DRAWING = 100,
+
+	POWER_SUSPEND_LEVEL_DISABLE_FB = 150,
+};
 struct power_suspend {
 	struct list_head link;
 	void (*suspend)(struct power_suspend *h);
@@ -50,7 +57,7 @@ void unregister_power_suspend(struct power_suspend *handler);
 void set_power_suspend_state_autosleep_hook(int new_state);
 void set_power_suspend_state_panel_hook(int new_state);
 
-extern bool power_suspended;
+//extern bool power_suspended;
 
 #endif
 
