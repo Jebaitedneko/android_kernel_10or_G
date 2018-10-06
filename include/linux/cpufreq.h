f2181840b91d8bb26da6b96a54584ba29c05f9a3
Rakesh Roshan <rakeshraimca@gmail.com>
2018-10-07 00:47:54 +0530

    upstreaming kernel to 3.18.123

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -619,6 +619,9 @@ extern struct cpufreq_governor cpufreq_gov_ragingmolasses;
 extern struct cpufreq_governor cpufreq_gov_ironactive;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ironactive)
 
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMANDX)
+extern struct cpufreq_governor cpufreq_gov_ondemandx;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemandx)
 
 #endif
 
