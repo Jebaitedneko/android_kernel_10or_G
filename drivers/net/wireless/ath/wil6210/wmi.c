1d313529d030e783cab4a07ade25e99b7efc43e7
Rakesh Roshan <rakeshraimca@gmail.com>
2018-12-22 08:09:49 +0530

    fixing some varibles issue

--- drivers/net/wireless/ath/wil6210/wmi.c
+++ drivers/net/wireless/ath/wil6210/wmi.c
@@ -1326,13 +1326,13 @@ int wmi_add_cipher_key(struct wil6210_priv *wil, u8 key_index,
 
 int wmi_set_ie(struct wil6210_priv *wil, u8 type, u16 ie_len, const void *ie)
 {
-	static const char *const names[] = {
+	/*static const char *const names[] = {
 		[WMI_FRAME_BEACON]	= "BEACON",
 		[WMI_FRAME_PROBE_REQ]	= "PROBE_REQ",
 		[WMI_FRAME_PROBE_RESP]	= "WMI_FRAME_PROBE_RESP",
 		[WMI_FRAME_ASSOC_REQ]	= "WMI_FRAME_ASSOC_REQ",
 		[WMI_FRAME_ASSOC_RESP]	= "WMI_FRAME_ASSOC_RESP",
-	};
+	};*/
 	int rc;
 	u16 len = sizeof(struct wmi_set_appie_cmd) + ie_len;
 	struct wmi_set_appie_cmd *cmd;
