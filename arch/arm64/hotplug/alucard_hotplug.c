003c682c85fe41e4b61f387cd56ee8a3504539ff
Rakesh Roshan <rakeshraimca@gmail.com>
2018-12-28 13:45:38 +0530

    Making Alucard enabled

--- arch/arm64/hotplug/alucard_hotplug.c
+++ arch/arm64/hotplug/alucard_hotplug.c
@@ -66,11 +66,7 @@ static struct hotplug_tuners {
 	struct mutex alu_hotplug_mutex;
 } hotplug_tuners_ins = {
 	.hotplug_sampling_rate = 30,
-#ifdef CONFIG_MACH_JF
 	.hotplug_enable = 1,
-#else
-	.hotplug_enable = 0,
-#endif
 	.min_cpus_online = 1,
 	.maxcoreslimit = NR_CPUS,
 	.maxcoreslimit_sleep = 1,
