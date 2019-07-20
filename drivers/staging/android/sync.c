7261ea034aebd7491c755cadc90c56ac907269e9
Sultan Alsawaf <sultan@kerneltoast.com>
2019-07-20 10:33:00 -0700

    staging: sync: Remove fence names by default
    Copying fence debug names for frequent users of sync fences causes a
    measurably significant portion of CPU time to be spent on the strlcpy.
    Additionally, the sync_fence struct is allocated very frequently, so
    wasting space on an unneeded 64-byte name is suboptimal.
    
    Remove the 64-byte name from the struct to reduce its size considerably
    and eliminate unnecessary operations on fence names.
    
    Signed-off-by: Sultan Alsawaf <sultan@kerneltoast.com>

--- drivers/staging/android/sync.c
+++ drivers/staging/android/sync.c
@@ -272,7 +272,6 @@ static struct sync_fence *sync_fence_alloc(const char *name)
 #ifdef CONFIG_SYNC_DEBUG
 	strlcpy(fence->name, name, sizeof(fence->name));
 #endif
-
 	INIT_LIST_HEAD(&fence->pt_list_head);
 	INIT_LIST_HEAD(&fence->waiter_list_head);
 	spin_lock_init(&fence->waiter_list_lock);
@@ -801,8 +800,10 @@ static long sync_fence_ioctl_fence_info(struct sync_fence *fence,
 	if (data == NULL)
 		return -ENOMEM;
 
+#ifdef CONFIG_SYNC_DEBUG
 	strlcpy(data->name, fence->name, sizeof(data->name));
 	data->status = fence->status;
+#endif
 	len = sizeof(struct sync_fence_info_data);
 
 	list_for_each(pos, &fence->pt_list_head) {
