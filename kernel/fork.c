81074147ab46e4e425d2c86a63e6ff759e09b0d3
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-09-30 21:19:24 +0700

    Revert "kernel: Fix massive cpufreq stats memory leaks"
    Prepare for complete cpufreq stats merge
    
    This reverts commit 70830011bd0cae029915c9e5f9f1861fe4c4fceb.

--- kernel/fork.c
+++ kernel/fork.c
@@ -75,7 +75,6 @@
 #include <linux/uprobes.h>
 #include <linux/aio.h>
 #include <linux/compiler.h>
-#include <linux/cpufreq.h>
 
 #include <asm/pgtable.h>
 #include <asm/pgalloc.h>
@@ -216,9 +215,6 @@ static void account_kernel_stack(struct thread_info *ti, int account)
 
 void free_task(struct task_struct *tsk)
 {
-#ifdef CONFIG_CPU_FREQ_STAT
-	cpufreq_task_stats_exit(tsk);
-#endif
 	account_kernel_stack(tsk->stack, -1);
 	arch_release_thread_info(tsk->stack);
 	free_thread_info(tsk->stack);
