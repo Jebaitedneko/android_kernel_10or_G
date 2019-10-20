2fbf5ee9c25f14d937d3534a1e3e18f514ccefbd
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-10-20 09:25:32 +0700

    cpuset: adopt for non-big.Little setup
    Well yeah, we're not big.Little too :'v
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- kernel/cpuset.c
+++ kernel/cpuset.c
@@ -1726,7 +1726,7 @@ static ssize_t cpuset_write_resmask_wrapper(struct kernfs_open_file *of,
 		{ "system-background",		"0-3"},
 		{ "restricted",			"0-3"},
 		{ "top-app",			"0-7"},
-		{ "foreground",			"0-3,6-7"},
+		{ "foreground",			"0-7"},
 	};
 	struct cpuset *cs = css_cs(of_css(of));
 	int i;
