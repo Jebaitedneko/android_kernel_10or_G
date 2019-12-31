232d564a5f3feef1411d2486638029631dc66c27
Dicky Herlambang <herlambangdicky5@gmail.com>
2019-12-31 19:37:52 +0700

    SQUASH: usb: quirks: pull other missing changes after upstream
    This is actually pick https://github.com/KudProject/kernel_xiaomi_msm8953-3.18/commit/5ad9d27c1244b3444de02396e1fffe99d21b8d90#diff-eb6865777795e5f716faa4c39e1afadb & https://github.com/KudProject/kernel_xiaomi_msm8953-3.18/commit/b3f13aba40ef73f86c23c3b3825f953d7d403f69
    which is missing after upstream, hope can solve the issue.
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- drivers/usb/core/quirks.c
+++ drivers/usb/core/quirks.c
@@ -154,6 +154,14 @@ static const struct usb_device_id usb_quirk_list[] = {
 	/* M-Systems Flash Disk Pioneers */
 	{ USB_DEVICE(0x08ec, 0x1000), .driver_info = USB_QUIRK_RESET_RESUME },
 
+	/* Baum Vario Ultra */
+	{ USB_DEVICE(0x0904, 0x6101), .driver_info =
+			USB_QUIRK_LINEAR_FRAME_INTR_BINTERVAL },
+	{ USB_DEVICE(0x0904, 0x6102), .driver_info =
+			USB_QUIRK_LINEAR_FRAME_INTR_BINTERVAL },
+	{ USB_DEVICE(0x0904, 0x6103), .driver_info =
+			USB_QUIRK_LINEAR_FRAME_INTR_BINTERVAL },
+
 	/* Keytouch QWERTY Panel keyboard */
 	{ USB_DEVICE(0x0926, 0x3333), .driver_info =
 			USB_QUIRK_CONFIG_INTF_STRINGS },
@@ -190,7 +198,8 @@ static const struct usb_device_id usb_quirk_list[] = {
 	{ USB_DEVICE(0x1b1c, 0x1b15), .driver_info = USB_QUIRK_DELAY_INIT },
 
 	/* Corsair Strafe RGB */
-	{ USB_DEVICE(0x1b1c, 0x1b20), .driver_info = USB_QUIRK_DELAY_INIT },
+	{ USB_DEVICE(0x1b1c, 0x1b20), .driver_info = USB_QUIRK_DELAY_INIT |
+	  USB_QUIRK_DELAY_CTRL_MSG },
 
 	/* Corsair K70 LUX RGB */
 	{ USB_DEVICE(0x1b1c, 0x1b33), .driver_info = USB_QUIRK_DELAY_INIT },
