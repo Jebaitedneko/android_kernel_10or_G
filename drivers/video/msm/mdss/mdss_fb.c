cf4871170e503da9e83adababe7541bf2ce6be53
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-01-02 05:59:16 +0700

    mdss_fb: disable backlight dimmer by default
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- drivers/video/msm/mdss/mdss_fb.c
+++ drivers/video/msm/mdss/mdss_fb.c
@@ -91,7 +91,7 @@
 #define MDSS_BRIGHT_TO_BL_DIM(out, v) do {\
 			out = ((v) * (v) * 255  / 4095 + (v) * (255 - (v)) / 32);\
 			} while (0)
-bool __read_mostly backlight_dimmer = true;
+bool __read_mostly backlight_dimmer = false;
 module_param(backlight_dimmer, bool, 0644);
 
 int backlight_min = 0;
