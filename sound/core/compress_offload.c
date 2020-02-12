09ce752bbb7022705a093206785c16394749cc7c
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-02-12 19:22:05 +0000

    ALSA: compress: selective merge for missing fix from https://github.com/KudProject/kernel_msm-3.18/commit/83eadcd6ecbb7f73aa6980d5b64814f325676f78
    Selective merge fix after 'ASB-2019-12-05_3.18' security patch issue
    
    --KrasCGQ Notes--
    1. Fix trivial merge conflicts due to commit 7a68763.
    2. Commit e7f182ad2eaa is already applied by CAF as commit bff23c7.
    
    Signed-off-by: Albert I <kras@raphielgang.org>
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- sound/core/compress_offload.c
+++ sound/core/compress_offload.c
@@ -719,12 +719,21 @@ static int snd_compr_drain(struct snd_compr_stream *stream)
 	int retval;
 
 	mutex_lock(&stream->device->lock);
-	if (stream->runtime->state == SNDRV_PCM_STATE_PREPARED ||
-			stream->runtime->state == SNDRV_PCM_STATE_SETUP) {
+	switch (stream->runtime->state) {
+	case SNDRV_PCM_STATE_OPEN:
+	case SNDRV_PCM_STATE_SETUP:
+	case SNDRV_PCM_STATE_PREPARED:
+	case SNDRV_PCM_STATE_PAUSED:
 		retval = -EPERM;
 		goto ret;
+	case SNDRV_PCM_STATE_XRUN:
+		retval = -EPIPE;
+		goto ret;
+	default:
+		break;
 	}
 	mutex_unlock(&stream->device->lock);
+
 	retval = stream->ops->trigger(stream, SND_COMPR_TRIGGER_DRAIN);
 	mutex_lock(&stream->device->lock);
 	if (!retval) {
@@ -764,12 +773,19 @@ static int snd_compr_partial_drain(struct snd_compr_stream *stream)
 	int retval;
 
 	mutex_lock(&stream->device->lock);
-	if (stream->runtime->state == SNDRV_PCM_STATE_PREPARED ||
-			stream->runtime->state == SNDRV_PCM_STATE_SETUP) {
-		mutex_unlock(&stream->device->lock);
+	switch (stream->runtime->state) {
+	case SNDRV_PCM_STATE_OPEN:
+	case SNDRV_PCM_STATE_SETUP:
+	case SNDRV_PCM_STATE_PREPARED:
+	case SNDRV_PCM_STATE_PAUSED:
 		return -EPERM;
+	case SNDRV_PCM_STATE_XRUN:
+		return -EPIPE;
+	default:
+		break;
 	}
 	mutex_unlock(&stream->device->lock);
+
 	/* stream can be drained only when next track has been signalled */
 	if (stream->next_track == false)
 		return -EPERM;
