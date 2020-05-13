b31a2c015a946c8ffa3817f984a1cc46f25bc7d0
Jebaitedneko <16777012+Jebaitedneko@users.noreply.github.com>
2020-05-13 07:48:38 +0530

    add missing actuator code

--- drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
+++ drivers/media/platform/msm/camera_v2/sensor/actuator/msm_actuator.c
@@ -1109,14 +1109,16 @@ static int32_t msm_actuator_power_down(struct msm_actuator_ctrl_t *a_ctrl)
 
 	CDBG("Enter\n");
 	if (a_ctrl->actuator_state != ACT_DISABLE_STATE) {
-
+		#ifdef CONFIG_HQ_ZQL1590_ACTUATOR
+				CDBG("no actuator_park_lens\n");			
+		#else 
 		if (a_ctrl->func_tbl && a_ctrl->func_tbl->actuator_park_lens) {
 			rc = a_ctrl->func_tbl->actuator_park_lens(a_ctrl);
 			if (rc < 0)
 				pr_err("%s:%d Lens park failed.\n",
 					__func__, __LINE__);
 		}
-
+		#endif
 		rc = msm_actuator_vreg_control(a_ctrl, 0);
 		if (rc < 0) {
 			pr_err("%s failed %d\n", __func__, __LINE__);
